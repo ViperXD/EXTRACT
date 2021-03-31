8a1a450bdea7e4170917b33e6a0ecf7dd2698365
TG Expert <78008481+v-v-r-official@users.noreply.github.com>
2021-03-31 18:52:13 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -24,7 +24,7 @@ async def start(client, message):
                 ],
                 [
                     InlineKeyboardButton(
-                        "⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/TroJanzHEX")
+                        "⭕️ JOIN OUR CHANNEL ⭕️", url="https://t.me/VKPROJECTS")
                 ]
             ]
         ),
@@ -45,7 +45,7 @@ async def help(client, message):
                 ],
                 [
                     InlineKeyboardButton(
-                        "⭕️ SUPPORT ⭕️", url="https://t.me/TroJanzSupport")
+                        "⭕️ SUPPORT ⭕️", url="https://t.me/VKP_BOTS")
                 ]
             ]
         ),
@@ -66,7 +66,7 @@ async def about(client, message):
                 ],
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/TroJanzHEX/Streams-Extractor")
+                        "JOIN ON CHANNEL", url="t.me/VKPROJECTS")
                 ]
             ]
         ),
